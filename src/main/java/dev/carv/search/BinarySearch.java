696b1cf523836f6daa136a801cec3ccf541b4af9
carolusquintus <carloschars@gmail.com>
2024-06-20 18:34:51 -0600

    Adding BinarySearch class

---
+++ src/main/java/dev/carv/search/BinarySearch.java
@@ -0,0 +1,12 @@
+package dev.carv.search;
+
+import java.util.List;
+
+public class BinarySearch<T> implements Search<T> {
+
+    @Override
+    public T search(T searchItem, List<T> items) {
+        return null;
+    }
+
+}
