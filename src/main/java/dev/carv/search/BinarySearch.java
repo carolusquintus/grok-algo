ff8f2e3350acad3b0698da29d1eac855da6ea0c6
carolusquintus <carloschars@gmail.com>
2024-06-23 19:00:04 -0600

    Adding BinarySearch implementation

--- src/main/java/dev/carv/search/BinarySearch.java
+++ src/main/java/dev/carv/search/BinarySearch.java
@@ -2,10 +2,27 @@ package dev.carv.search;
 
 import java.util.List;
 
-public class BinarySearch<T> implements Search<T> {
+public class BinarySearch<T extends Comparable<T>> implements Search<T> {
 
     @Override
     public T search(T searchItem, List<T> items) {
+        int low = 0;
+        int high = items.size() - 1;
+
+        while (low <= high) {
+            int middle = (low + high) / 2;
+            var guess = items.get(middle);
+
+            int comparison = guess.compareTo(searchItem);
+
+            if (comparison > 0) {
+                high = middle - 1;
+            } else if (comparison < 0) {
+                low = middle + 1;
+            } else { // Equivalent to (comparison == 0)
+                return guess;
+            }
+        }
         return null;
     }
 
