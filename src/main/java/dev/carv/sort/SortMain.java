4739f3d9ed74c6e930fec9c36b4ee3f052b1979d
carolusquintus <carloschars@gmail.com>
2024-06-24 18:16:21 -0600

    SortMain with Generator's call

--- src/main/java/dev/carv/sort/SortMain.java
+++ src/main/java/dev/carv/sort/SortMain.java
@@ -1,11 +1,10 @@
 package dev.carv.sort;
 
+import com.github.javafaker.Faker;
 import dev.carv.generator.AsyncGenerator;
-import dev.carv.generator.NameGenerator;
+import dev.carv.generator.LinearGenerator;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.time.StopWatch;
-
-import java.util.concurrent.ForkJoinPool;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
@@ -13,24 +12,22 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 public class SortMain {
 
     public static void main(String[] args) {
-        var generator = new NameGenerator();
+        Faker faker = new Faker();
 
+        var generator = new LinearGenerator<String>();
         var watch = new StopWatch();
         watch.start();
-        var names = generator.generate(5_000);
+        var names = generator.apply(5_000, () -> faker.name().name());
         watch.stop();
-
         log.debug("Total: {}", names.size());
         log.debug("Time {}ms", watch.getTime(MILLISECONDS));
 
-        var pool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
-        var parallel = new AsyncGenerator(0, 5_000);
-        watch.reset();
+        var asyncGenerator = new AsyncGenerator<String>();
+        watch = new StopWatch();
         watch.start();
-        pool.invoke(parallel);
+        var usernames = asyncGenerator.apply(5_000, () -> faker.name().username());
         watch.stop();
-        names = parallel.getResult();
-        log.debug("Total: {}", names.size());
+        log.debug("Total: {}", usernames.size());
         log.debug("Time {}ms", watch.getTime(MILLISECONDS));
     }
 
